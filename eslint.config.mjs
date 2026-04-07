c00ac068bf791b7f296712c55a9e7afcd3d5d170
lonewolfyx <877085893@qq.com>
2026-04-07 15:27:59 +0800

    style(lint): disable vue no-v-html rule in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,6 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
+        'vue/no-v-html': 'off',
     },
 })
