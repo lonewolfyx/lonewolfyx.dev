7d609d844609fec1aa8e5079c00c9a2cb3cecc03
lonewolfyx <877085893@qq.com>
2026-04-07 15:38:55 +0800

    refactor(lint): migrate to antfu eslint config with enhanced styling rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,32 @@
+import { antfu } from '@antfu/eslint-config'
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
+export default antfu({
+    stylistic: {
+        indent: 4,
+        quotes: 'single',
+    },
+    vue: {
+        overrides: {
+            'vue/block-order': ['error', {
+                order: [
+                    'template',
+                    'script',
+                    'style',
+                ],
+            }],
+        },
+    },
+    yaml: {
+        overrides: {
+            'yaml/indent': ['error', 2],
+        },
+    },
     rules: {
+        'e18e/prefer-static-regex': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         'vue/no-v-html': 'off',
+        'regexp/no-unused-capturing-group': 'off',
     },
-})
+}).append(withNuxt)
