2fc05570be974edb24377acfc2b5f2ec620ff28f
OldDriver <olddrivero.king@qq.com>
2026-05-08 16:11:51 +0800

    feat(layout): add scrollbar hiding functionality to default layout (#24)

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@
             <Times />
             <ScreenSize />
 
-            <div class="relative flex w-full min-h-dvh max-h-dvh overflow-auto flex-col bg-grid">
+            <div class="relative flex w-full min-h-dvh max-h-dvh overflow-auto flex-col bg-grid no-scrollbar">
                 <slot />
             </div>
         </main>
@@ -15,4 +15,13 @@
 <script lang="ts" setup>
 import ScreenSize from '~/components/features/ScreenSize.vue'
 import Times from '~/components/features/Times.vue'
+
+useHead({
+    htmlAttrs: {
+        class: 'route-hide-scrollbar',
+    },
+    bodyAttrs: {
+        class: 'route-hide-scrollbar',
+    },
+})
 </script>
