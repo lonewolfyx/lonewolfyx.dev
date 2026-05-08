afefda9da46af9d951320f240c3e825da5030c6d
lonewolfyx <olddrivero.king@qq.com>
2026-05-08 16:37:03 +0800

    fix(stack): update stack component styling and icon references

--- app/components/Stack.vue
+++ app/components/Stack.vue
@@ -10,26 +10,21 @@
                         'flex items-center gap-2',
                         'text-xs font-medium',
                         'rounded-md px-2 py-1.5',
-                        item.theme.background || 'bg-secondary',
+                        'bg-secondary',
                     )"
                     :to="item.href"
-                    target="_blank"
                     rel="noopener"
+                    target="_blank"
                 >
                     <Icon
-                        :name="item.icon"
-                        mode="svg"
                         :class="cn(
                             'size-3',
-                            item.theme.icon || 'text-white',
+                            item.theme?.icon || '',
                         )"
+                        :name="item.icon"
+                        mode="svg"
                     />
-                    <span
-                        :class="cn(
-                            'capitalize',
-                            item.theme.text || 'text-white',
-                        )"
-                    >{{ item.name }}</span>
+                    <span class="capitalize text-neutral-800 dark:text-neutral-300/80">{{ item.name }}</span>
                 </NuxtLink>
             </div>
         </div>
@@ -47,159 +42,90 @@ const stack = {
     frontend: [
         {
             name: 'Javascript',
-            icon: 'simple-icons:javascript',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-                background: 'bg-[#f7df1e]',
-            },
+            icon: 'logos:javascript',
             href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
         },
         {
             name: 'Tavascript',
-            icon: 'file-icons:typescript-alt',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#3178C6]',
-            },
+            icon: 'devicon:typescript',
             href: 'https://www.typescriptlang.org/',
         },
         {
             name: 'Vue',
-            icon: 'flowbite:vue-solid',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#4FC08D]',
-            },
+            icon: 'logos:vue',
             href: 'https://vuejs.org/',
         },
         {
             name: 'Nuxt',
-            icon: 'lineicons:nuxt',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#00DC82]',
-            },
+            icon: 'devicon:nuxt',
             href: 'https://nuxt.com/',
         },
         {
             name: 'React',
-            icon: 'simple-icons:react',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-                background: 'bg-[#61DAFB]',
-            },
+            icon: 'logos:react',
             href: 'https://react.dev/',
         },
         {
             name: 'Tailwind Css',
-            icon: 'mdi:tailwind',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#06B6D4]',
-            },
+            icon: 'material-icon-theme:tailwindcss',
             href: 'https://tailwindcss.com/',
         },
         {
             name: 'shadcn/ui/vue',
             icon: 'simple-icons:shadcnui',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#41B883]',
-            },
             href: 'https://shadcn-vue.com/',
         },
         {
             name: 'TanStack',
             icon: 'simple-icons:tanstack',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-            },
             href: 'https://tanstack.com/',
         },
         {
             name: 'pnpm',
-            icon: 'file-icons:pnpm',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-                background: 'bg-[#f8ab00]',
-            },
+            icon: 'devicon:pnpm',
             href: 'https://pnpm.io/',
         },
         {
             name: 'Node.js',
-            icon: 'simple-icons:npm',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#cb3837]',
-            },
+            icon: 'devicon:nodejs',
             href: 'https://nodejs.org/',
         },
         {
+            name: 'npm',
+            icon: 'material-icon-theme:npm',
+            href: 'https://npmjs.org/',
+        },
+        {
             name: 'Bun',
-            icon: 'devicon-plain:bun',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-black dark:bg-secondary',
-            },
+            icon: 'logos:bun',
             href: 'https://bun.sh/',
         },
         {
             name: 'Nitro',
             icon: 'unjs:nitro',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-gradient-to-r from-[#31b2f3] via-[#f27cec] to-[#fd6641]',
-            },
             href: 'https://nitro.build/',
         },
         {
             name: 'h3',
             icon: 'unjs:h3',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-            },
             href: 'https://h3.dev/',
         },
         {
             name: 'vite',
-            icon: 'simple-icons:vite',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#8900ff]',
-            },
+            icon: 'vscode-icons:file-type-vite',
             href: 'https://vite.dev/',
         },
         {
             name: 'cac',
             icon: 'tabler:hexagon',
             theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#8b7ee7]',
+                icon: 'text-[#8b7ee7]',
             },
             href: 'https://github.com/cacjs/cac',
         },
         {
             name: 'citty',
             icon: 'unjs:citty',
-            theme: {
-                icon: 'text-dark',
-                text: 'text-dark',
-            },
             href: 'https://unjs.io/packages/citty',
         },
     ],
@@ -207,32 +133,20 @@ const stack = {
         // ClaudeCode Codex Gemini QianWen
         {
             name: 'Claude Code',
-            icon: 'simple-icons:claude',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#ff7043]',
-            },
+            icon: 'material-icon-theme:claude',
             href: 'https://claude.ai/',
         },
         {
             name: 'codex',
             icon: 'icon:codex',
             theme: {
-                icon: 'fill-white',
-                text: 'text-white',
-                background: 'bg-black dark:bg-secondary',
+                icon: 'bg-clip-text bg-gradient-to-b from-[#B1A7FF] via-[#7A9DFF] to-[#3941FF] text-transparent',
             },
             href: 'https://chatgpt.com/codex/cloud',
         },
         {
             name: 'gemini',
-            icon: 'lineicons:gemini',
-            theme: {
-                icon: 'text-white',
-                text: 'text-white',
-                background: 'bg-[#00b95c]',
-            },
+            icon: 'vscode-icons:file-type-gemini',
             href: 'https://gemini.google.com/',
         },
     ],
